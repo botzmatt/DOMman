5f90b7572761dec464e3ea159e69e46cdacaf509
Matt Botz and J. Ryan Conklin <botzmatt@yahoo.com>
2016-05-23 19:00:28 -0700

    add webpages response to button clicks by user

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
   });
 
   $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye Dear User!</li>");
   });
 
   $("button#stop").click(function(){
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>I'm sorry. No offense was meant!</li>")
   });
 });
